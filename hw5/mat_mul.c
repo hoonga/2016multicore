0cf69b6ae9ab06febdc6ce3b35bf00ac4c89c535
hoonga <etaphirho@snu.ac.kr>
2016-11-15 04:58:02 +0900

    fixed matmul for only transpose

--- hw5/mat_mul.c
+++ hw5/mat_mul.c
@@ -9,7 +9,6 @@
 
 int print_matrix = 0;
 int validation = 0;
-#ifdef TRANSMUL
 const char * ktranspose =
 "__kernel void transpose(__global const float* src, __global float* dest, \n"
 "__local float *buf, int NDIM) {\n"
@@ -34,19 +33,6 @@ const char * kmat_mult =
 "        sum += A[X/D*D + k] * B[Y + k];\n"
 "    C[X + x] = sum;\n"
 "}";
-#else
-const char * kmat_mul =
-"__kernel void mat_mul(__global const float* A, __global const float* B, \n"
-"__global float* C, int D) {\n"
-"    size_t X = get_group_id(0)*64;\n"
-"    size_t x = get_local_id(0);\n"
-"    int k = 0;\n"
-"    float sum = 0;\n"
-"    for(;k < D; k++)\n"
-"        sum += A[X/D*D + k] * B[k*D+(X&(D-1))+x];\n"
-"    C[X+x] = sum;\n"
-"}";
-#endif
 
 void mat_mul( float * c, float * a, float * b, int NDIM )
 {
@@ -65,11 +51,8 @@ void mat_mul( float * c, float * a, float * b, int NDIM )
         cl_mem A, B, C;
         A = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, size, a, &res); where++; if( res != CL_SUCCESS ) break; // 5
         B = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, size, b, &res); where++; if( res != CL_SUCCESS ) break; // 6
-#ifdef TRANSMUL
         cl_mem BT = clCreateBuffer(context, CL_MEM_READ_WRITE, size, NULL, &res); where++; if( res != CL_SUCCESS ) break; // 7
-#endif
         C = clCreateBuffer(context, CL_MEM_WRITE_ONLY, size, NULL, &res); where++; if( res != CL_SUCCESS ) break; // 8
-#ifdef TRANSMUL
         size_t len = strlen(ktranspose);
         cl_program ptrans = clCreateProgramWithSource(context, 1, (const char**) &ktranspose, &len, &res); where++; if( res != CL_SUCCESS ) break;// 9
         res = clBuildProgram(ptrans, 1, device, NULL, NULL, NULL); where++; if( res != CL_SUCCESS ) break; // 10
@@ -94,22 +77,7 @@ void mat_mul( float * c, float * a, float * b, int NDIM )
         res = clEnqueueNDRangeKernel(q, trans, 2, NULL, tg, tl, 0, NULL, NULL); where++; if( res != CL_SUCCESS ) break;
         size_t global[] = {NDIM * NDIM};
         size_t local[] = {8 * 8};
-        printf("enque mul\n");
         res = clEnqueueNDRangeKernel(q, mult, 1, NULL, global, local, 0, NULL, NULL); where++; if( res != CL_SUCCESS ) break;
-#else
-        size_t len = strlen(kmat_mul);
-        cl_program program = clCreateProgramWithSource(context, 1, (const char**) &kmat_mul, &len, &res); where++; if( res != CL_SUCCESS ) break;
-        res = clBuildProgram(program, 1, device, NULL, NULL, NULL); where++; if( res != CL_SUCCESS ) break;
-        cl_kernel mul = clCreateKernel(program, "mat_mul", &res); where++; if( res != CL_SUCCESS ) break;
-        clSetKernelArg(mul, 0, sizeof(cl_mem), (void*) &A);
-        clSetKernelArg(mul, 1, sizeof(cl_mem), (void*) &B);
-        clSetKernelArg(mul, 2, sizeof(cl_mem), (void*) &C);
-        clSetKernelArg(mul, 3, sizeof(int), (void*) &(NDIM));
-        size_t global[] = {NDIM * NDIM};
-        size_t local[] = {8 * 8};
-        res = clEnqueueNDRangeKernel(q, mul, 1, NULL, global, local, 0, NULL, NULL); where++; if (res != CL_SUCCESS) break;
-#endif
-        printf("done");
         clEnqueueReadBuffer(q, C, CL_TRUE, 0, size, c, 0, NULL, NULL);
     } while(0);
     if (res != CL_SUCCESS) printf("failed with : %d at %d \n", res, where);
